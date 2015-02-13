f697a75a3456bcf49c5985281b373e03c2aa4b95
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-13 10:52:47 -0600

    updates

--- tasks/comn.js
+++ tasks/comn.js
@@ -1,17 +1,11 @@
 module.exports = function(grunt) {
     var comn = require("comn"),
-        filePath = require("file_path"),
         fileUtils = require("file_utils");
 
     grunt.registerMultiTask("comn", "Compiles CommonJS modules into one file", function() {
         var options = this.options(),
-            index = options.index || options.main || options.file,
-            files = comn(index, options),
-            out = options.out,
-            path;
+            index = options.index || options.main || options.file;
 
-        for (path in files) {
-            fileUtils.writeFileSync(filePath.join(out, path), files[path]);
-        }
+        fileUtils.writeFileSync(options.out, comn(index, options));
     });
 };
